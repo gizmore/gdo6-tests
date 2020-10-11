07d384f4f297dd72f35351ef801e5b418dbd5ffa
gizmore <gizmore@wechall.net>
2020-10-11 13:19:23 +0200

    6.10-r9100

---
+++ Module_Tests.php
@@ -0,0 +1,10 @@
+<?php
+namespace GDO\Tests;
+
+use GDO\Core\GDO_Module;
+
+final class Module_Tests extends GDO_Module
+{
+    public $module_priority = 10;
+    
+}
---
+++ Test/TestTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace GDO\Tests\Test;
+
+use PHPUnit\Framework\TestCase;
+
+final class TestTest extends TestCase
+{
+    public function testMethodCall()
+    {
+        
+    }
+}
+
