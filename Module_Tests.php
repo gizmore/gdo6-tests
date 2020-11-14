cb0d74701f3ddaaf4ee33e439501b156d3d5ba04
gizmore <gizmore@wechall.net>
2020-11-14 18:01:28 +0100

    6.10-r9378_tables

--- Module_Tests.php
+++ Module_Tests.php
@@ -3,6 +3,14 @@ namespace GDO\Tests;
 
 use GDO\Core\GDO_Module;
 
+/**
+ * Module that generates Tests from Methods automatically.
+ * A good start to try many code paths.
+ * 
+ * @author gizmore
+ * @version 6.10
+ * @since 6.10
+ */
 final class Module_Tests extends GDO_Module
 {
     public $module_priority = 100;
