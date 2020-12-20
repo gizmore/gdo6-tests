7eb139fc9c84da21f7671c302439712de58f1ae9
gizmore <gizmore@wechall.net>
2020-12-20 10:50:32 +0100

    6.10-r9388

--- MethodTest.php
+++ MethodTest.php
@@ -1,8 +1,14 @@
 <?php
 namespace GDO\Tests;
 
+use GDO\Core\GDT;
 use GDO\Core\Method;
 use GDO\User\GDO_User;
+use GDO\DB\GDT_String;
+use GDO\Util\Classes;
+use GDO\DB\GDT_Int;
+use GDO\DB\GDT_Decimal;
+use function PHPUnit\Framework\assertTrue;
 
 /**
  * Helper Class to test a gdo method.
@@ -62,11 +68,73 @@ final class MethodTest
         $_POST[$btn] = $btn;
         $_REQUEST[$btn] = $btn;
         
-        $response = $this->method->execute();
+        $this->method->init();
         
-        assert($response->code === 200);
+        $response = $this->method->exec();
+        
+        assertTrue($response->code === 200);
         
         return $response;
     }
     
+    ################################
+    ### Automatic Method Testing ###
+    ################################
+    /**
+     * Try to plug default values for a method and test it.
+     * @param string $moduleName
+     * @param string $methodName
+     * @param array $parameters
+     * @return self
+     */
+    public function defaultMethod($moduleName, $methodName, $parameters=[], $button='submit')
+    {
+        $method = method($moduleName, $methodName);
+        
+        # Plug default params
+        foreach ($method->gdoParameterCache() as $name => $gdt)
+        {
+            if ($gdt->notNull && $gdt->getVar() === null)
+            {
+                if (!isset($parameters[$name]))
+                {
+                    $parameters[$name] = $this->plugParam($gdt);
+                }
+            }
+        }
+        
+        # Exec
+        return self::make()->method($method)->parameters($parameters)->execute($button);
+    }
+    
+    /**
+     * Try to guess default params for a GDT.
+     * @param GDT $gdt
+     * @return string
+     */
+    private function plugParam(GDT $gdt)
+    {
+        # Select first object
+        if (Classes::class_uses_trait($gdt, 'GDO\\DB\\WithObject'))
+        {
+            return $gdt->gdo->table()->select()->first()->exec()->fetchObject()->getID();
+        }
+        
+        # Title and description
+        if ($gdt instanceof GDT_String)
+        {
+            return "Test String <script>alert(1);</script>";
+        }
+        
+        if ($gdt instanceof GDT_Int)
+        {
+            return "4"; # chosen by fair avg dice roll.
+        }
+        
+        if ($gdt instanceof GDT_Decimal)
+        {
+            return "3.14";
+        }
+    }
+
 }
